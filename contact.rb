824bfcd112d25342f93d6e16b3019f1d31dacb57
mikesmall <michael.l.a.small@gmail.com>
2017-05-24 16:42:17 -0400

    More progress on Contact class

--- contact.rb
+++ contact.rb
@@ -19,40 +19,58 @@ class Contact
     new_contact = Contact.new(first_name, last_name, email, note)
     @@contacts << new_contact
     return new_contact
-  end
+  end#self.create
 
   # This method should return all of the existing contacts
   def self.all
-    @@contacts
-    # from AM lesson code
-  end
+    @@contacts # from AM lesson code
+  end#self.all
 
   # This method should accept an id as an argument
   # and return the contact who has that id
   def self.find
-  end
+  end#self.find
 
   # This method should allow you to specify
   # 1. which of the contact's attributes you want to update
   # 2. the new value for that attribute
   # and then make the appropriate change to the contact
   def update
-  end
+    puts "What would you like to update?"
+    puts "First Name -- press 'f'"
+    puts "Last Name -- press 'l'"
+    puts "Email -- press 'e'"
+    puts "Note -- press 'n'"
+    puts "(and then press Enter)"
+    choice = gets.chomp
+    if choice = f
+      first_name = gets.chomp
+    elsif choice = l
+      last_name = gets.chomp
+    elsif choice = e
+      email = gets.chomp
+    elsif choice = n
+      note = gets.chomp
+    else
+      puts "What?! Please choose from the letters above."
+      # I need a way to re-start this method here.
+    end#if
+  end#update
 
   # This method should work similarly to the find method above
   # but it should allow you to search for a contact using attributes other than id
   # by specifying both the name of the attribute and the value
   # eg. searching for 'first_name', 'Betty' should return the first contact named Betty
   def self.find_by
-  end
+  end#self.find_by
 
   # This method should delete all of the contacts
   def self.delete_all
-  end
+    @@contacts.clear # should be all I need here.
+  end#self.delete_all
 
   def full_name
-    "#{first_name} #{last_name}"
-    # From AM lesson code
+    "#{first_name} #{last_name}" # From AM lesson code
   end
 
   # This method should delete the contact
