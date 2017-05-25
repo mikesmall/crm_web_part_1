5f0c148fc0856545d9eee1ae51463229d08b4fda
mikesmall <michael.l.a.small@gmail.com>
2017-05-25 14:05:30 -0400

    Moving through CRM Class steps

--- crm.rb
+++ crm.rb
@@ -1,4 +1,4 @@
-require_relative 'contact.rb' # I added this. Too early to know for sure if it belongs here.
+require_relative 'contact.rb'
 
 class CRM
 
@@ -6,16 +6,48 @@ class CRM
   end
 
   def main_menu
-    # As a user, upon starting the CRM, I am presented with a prompt to add, modify, delete, display all, search by attribute, and exit.
-  end
-  def print_main_menu
-  end
+    while true # repeat indefinitely
+      print_main_menu
+      user_selected = gets.to_i
+      call_option(user_selected)
+    end#while
+  end#main_menu
 
-  def call_option
-  end
+  def print_main_menu
+    puts '[1] Add a new contact'
+    puts '[2] Modify an existing contact'
+    puts '[3] Delete a contact'
+    puts '[4] Display all the contacts'
+    puts '[5] Search by attribute'
+    puts '[6] Exit'
+    puts 'Enter a number: '
+  end#print_main_menu
+
+  def call_option(user_selected)
+    case user_selected
+    when 1 then add_new_contact
+    when 2 then modify_existing_contact
+    # Finish off the rest for 3 through 6
+    when 3 then delete_contact
+    when 4 then display_all_contacts
+    when 5 then search_by_attribute
+    when 6 then exit_program
+    #  To be clear, the methods add_new_contact and modify_existing_contact
+    #  haven't been implemented yet
+    end#case
+  end#call_option
 
   def add_new_contact
-  end
+    print 'Enter First Name: '
+    first_name = gets.chomp
+    print 'Enter Last Name: '
+    last_name = gets.chomp
+    print 'Enter Email Address: '
+    email = gets.chomp
+    print 'Enter a Note: '
+    note = gets.chomp
+    Contact.create(first_name, last_name, email, note)
+  end#add_new_contact
 
   def modify_existing_contact
     # As a user, when I enter the id of the user I want to modify I am then prompted to select which attribute I want to change from the list 'first name', 'last name', 'email', or 'note'.
@@ -36,8 +68,9 @@ class CRM
     # As a user, when I enter the search term I am then presented with the first contact who matches my search.
   end
 
-  def exit
+  def exit_program
     # As a user, if I select exit I am returned to the command line.
+    abort('Exiting program. Bye!')
   end
 
 
