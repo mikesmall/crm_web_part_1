95c55f5ace20af2a6de73be1787736d0b53a8cf2
mikesmall <michael.l.a.small@gmail.com>
2017-06-04 16:53:52 -0400

    Updated Contact.create code in crm.rb per Step 2

--- crm.rb
+++ crm.rb
@@ -65,7 +65,12 @@ class CRM
     email = gets.chomp
     print 'Enter a Note: '
     note = gets.chomp
-    Contact.create(first_name, last_name, email, note)
+    contact = Contact.create(
+      first_name: first_name,
+      last_name:  last_name,
+      email:      email,
+      note:       note
+    )
   end
 
   def modify_existing_contact
