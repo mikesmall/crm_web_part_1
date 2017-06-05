9aeb3223029ea6e967f8a70a89c656764ebaecbd
mikesmall <michael.l.a.small@gmail.com>
2017-06-05 15:51:13 -0400

    CRM Web Part 4: Completed 'editing a contact' step

--- crm.rb
+++ crm.rb
@@ -40,6 +40,32 @@ post '/contacts' do
   redirect to('/contacts')
 end#post-contacts
 
+get '/contacts/:id/edit' do
+  @contact = Contact.find(params[:id].to_i)
+  if @contact
+    erb :edit_contact
+  else
+    raise Sinatra::NotFound
+  end
+end
+
+put '/contacts/:id' do
+  @contact = Contact.find(params[:id].to_i)
+  if @contact
+    @contact.update(
+    first_name: params[:first_name],
+    last_name:  params[:last_name],
+    email:      params[:email],
+    note:       params[:note]
+    )
+
+    redirect to('/contacts')
+  else
+    raise Sinatra::NotFound
+  end#if
+end#contacts/:id
+
+# Keep this at the bottom:
 after do
   ActiveRecord::Base.connection.close
 end#after
